06e0e4f045b2f4e48497e92cc8519408fbba9b8b
Wallace H Wu <wallace_h_wu@wistron.com>
2020-06-12 16:18:46 +0800

    update root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-starter';
+  title = 'Angular-8-starter-kit is working!';
 }
